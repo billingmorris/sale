17af28f761bcc5e1bdb56d2bfcdc229eb4941c61
Bill Morris <billingmorris@gmail.com>
2026-07-23 15:49:54 -0500

    Refactor action_create_quotation to use lines

--- product_create_quotation/models/product_template.py
+++ product_create_quotation/models/product_template.py
@@ -5,34 +5,25 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     def action_create_quotation(self):
-        self.ensure_one() if len(self) == 1 else None
-
-        order_lines = []
+        lines = []
 
         for template in self:
-            # Obtener la variante correspondiente
             product = template.product_variant_id
 
             if product:
-                order_lines.append((0, 0, {
+                lines.append((0, 0, {
                     "product_id": product.id,
-                    "name": product.get_product_multiline_description_sale(),
                     "product_uom_qty": 1.0,
                     "product_uom": product.uom_id.id,
-                    "price_unit": product.lst_price,
                 }))
-
-        context = dict(self.env.context)
-        context.update({
-            "default_order_line": order_lines,
-        })
 
         return {
             "type": "ir.actions.act_window",
             "name": _("Nueva cotización"),
             "res_model": "sale.order",
             "view_mode": "form",
-            "views": [(False, "form")],
             "target": "current",
-            "context": context,
+            "context": {
+                "default_order_line": lines,
+            },
         }
