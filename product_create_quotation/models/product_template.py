4c6a05d5dadd0233bdad8d4c97e9a94dbd60d442
Bill Morris <billingmorris@gmail.com>
2026-07-23 15:05:29 -0500

    Refactor action_create_quotation method
    Refactor action_create_quotation to use lines list for order lines.

--- product_create_quotation/models/product_template.py
+++ product_create_quotation/models/product_template.py
@@ -1,34 +1,23 @@
 from odoo import models, _
-from odoo.exceptions import UserError
-
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     def action_create_quotation(self):
-        if not self:
-            raise UserError(_("Debe seleccionar al menos un producto."))
-
-        order = self.env["sale.order"].create({})
-
+        lines = []
         for template in self:
             product = template.product_variant_id
-            if not product:
-                raise UserError(
-                    _("El producto '%s' no tiene una variante disponible.") % template.display_name
-                )
-
-            self.env["sale.order.line"].create({
-                "order_id": order.id,
-                "product_id": product.id,
-                "product_uom_qty": 1.0,
-            })
-
+            if product:
+                lines.append((0, 0, {
+                    "product_id": product.id,
+                    "product_uom_qty": 1.0,
+                    "product_uom": product.uom_id.id,
+                }))
         return {
             "type": "ir.actions.act_window",
-            "name": _("Cotización"),
+            "name": _("Nueva cotización"),
             "res_model": "sale.order",
-            "res_id": order.id,
             "view_mode": "form",
             "target": "current",
+            "context": {"default_order_line": lines},
         }
